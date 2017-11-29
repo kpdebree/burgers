2c94f830ddb03fc8b81b157fc9243f20fdfdb510
kpdebree <kpdebree@gmail.com>
2017-11-28 18:23:50 -0800

    Windows Heroku Command Line

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
 var mysql = require("mysql");
-var password = require("../password")
 
 var connection = mysql.createConnection({
   host: "localhost" || "q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user:  "root" || "se6411lddxbli7gd",
-  password: password || "frq202dgbyp25evw" || "123Mercutio456",
+  password: "" || "frq202dgbyp25evw"
   database: "burgers_db"
 });
 
